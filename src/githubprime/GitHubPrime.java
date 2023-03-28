3b502a6bee648d45b7c37e39ff9a1afda25cc4a8
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-03-28 14:11:30 +0100

    Change print to "Goodbye Moon!"
    Deleted Hello world and change to Goodbye Moon

--- src/githubprime/GitHubPrime.java
+++ src/githubprime/GitHubPrime.java
@@ -14,7 +14,7 @@ public class GitHubPrime {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Goodbye Moon!");
     }
     
 }
